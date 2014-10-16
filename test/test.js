12fb6228213577ae753145c7bc8f1603a8f301c9
Joseph Dykstra <josephdykstra@gmail.com>
2014-10-16 17:54:13 -0500

    Refactor everything into index.js, made it work

--- test/test.js
+++ test/test.js
@@ -1,7 +1,36 @@
 ﻿var test = require('tap').test
-var index = require('../index.js')
 
-test('test for index', function(t) {
+test('test for index', function (t) {
+	var index = require('../index.js')
+
+	index()
+
+	t.end()
+})
+
+test('test for processDir', function (t) {
+	var processDir = require('../processDir.js')
+	var count = 0;
+	processDir(__dirname, function (fileText, packageJson) { //or process.cwd()
+		count++;
+		t.equal(
+			fileText.replace(/\r/g, '').slice(0, 50),
+			"debouncer\n=============\n\n{{table-of-contents}}\n\n##",
+			"got correct text"
+		)
+	})
+	t.equal(count, 1, "Processed every file")
+
+	t.end()
+})
+
+test('test for processFile', function (t) {
+	var processFile = require('../processFile.js')
+
+	processFile("{{license.mit.text}}", {
+		organization: "ArtskydJ",
+		project: "md-utils"
+	})
 
 	t.end()
 })
